13e5ca3f937019bdce292f1a51aa752b6285f5ce
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2005-11-18 17:18:36 -0200

    Letroca v0.05

--- letroca.c
+++ letroca.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include <allegro.h>
 
 /* Constantes */
@@ -15,6 +16,7 @@
 /* Globais */
 int som = 1;
 int janela = 0;
+int score = 0;
 volatile int ctrl_vel;
 volatile int tim_tempo;
 
@@ -31,7 +33,7 @@ struct subgrupos
 	/*int indice;*/
 	int numletras;
 	char palavra[16];
-	int ligado[16];
+	int ligado;
 };
 
 /* Escopos */
@@ -61,9 +63,13 @@ int main (int argc, char **argv)
 	{
 		for ( i = 1 ; i < argc ; i++ )
 		{
-			if ( !strcmpi(argv[i],"-janela") )
+			if ( !strcasecmp(argv[i],"-janela") )
 			{
 				janela = 1;
+			}
+			if ( !strcasecmp(argv[i],"-semsom") )
+			{
+				som = 0;
 			}
 		}
 	}
@@ -144,12 +150,12 @@ void jogo (void)
 	/* Carrega todas as letras na memória */
 	for ( i = 0 ; i < LETRAS ; i++ )
 	{
-		sprintf(nome,"%c.bmp",'A'+i);
+		sprintf(nome,"./imagens/%c.bmp",'A'+i);
 		letras[i] = load_bitmap(nome,NULL);
 	}
 	
 	buffer = create_bitmap(MAX_X, MAX_Y);
-	fundo = load_bitmap("back.bmp",NULL);
+	fundo = load_bitmap("./imagens/back.bmp",NULL);
 	if ( !buffer )
 	{
 		return;
@@ -171,9 +177,8 @@ void jogo (void)
 
 void intro (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 {
-	int fade,i, j,k;
+	int fade, i, j, k, pressed;
 	BITMAP *letras[NUM_LET];
-	char nome[20];
 	
 	/* Carrega os sprites das palavras na memória 
 	for ( i = 0 ; i < NUM_LET ; i++ )
@@ -241,7 +246,8 @@ void intro (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 			blit(buffer, screen, 0, 0, 0, 0, buffer->w, buffer->h);
 			if ( keypressed() )
 			{
-				if ( ( readkey() >> 8 ) == KEY_ENTER )
+				pressed = readkey();
+				if ( ( pressed >> 8 ) == KEY_ENTER || ( pressed >> 8 ) == KEY_ENTER_PAD )
 				{
 					break;
 				}
@@ -262,11 +268,11 @@ void menu (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 	menul[3] = Letras['U'-'A'];
 	
 	/* carrega os botões na memória */
-	menub = load_bitmap("menu.bmp",NULL);
-	sairb  = load_bitmap("sair.bmp",NULL);
-	sobreb = load_bitmap("sobre.bmp",NULL);
-	jogob = load_bitmap("jogo.bmp",NULL);
-	jogotempob = load_bitmap("jogotempo.bmp",NULL);
+	menub = load_bitmap("./imagens/menu.bmp",NULL);
+	sairb  = load_bitmap("./imagens/sair.bmp",NULL);
+	sobreb = load_bitmap("./imagens/sobre.bmp",NULL);
+	jogob = load_bitmap("./imagens/jogo.bmp",NULL);
+	jogotempob = load_bitmap("./imagens/jogotempo.bmp",NULL);
 	
 	sizex = sairb->w;
 	sizey = sairb->h;
@@ -298,6 +304,7 @@ void menu (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 				if ( mouse_y >= 161 && mouse_y <= 161+sizey )
 				{
 					letroca (buffer, fundo, Letras, 0);
+					score = 0;
 				}
 				/* se está sobre o "sobre" */
 				else if ( mouse_y >= 314 && mouse_y <= 314+sizey )
@@ -313,6 +320,7 @@ void menu (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 				if ( mouse_y >= 237 && mouse_y <= 237+sizey )
 				{
 					letroca (buffer, fundo, Letras, 1);
+					score = 0;
 				}
 				/* se está sobre o "sair" */
 				else if ( mouse_y >= 392 && mouse_y <= 392+sizey )
@@ -320,6 +328,7 @@ void menu (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 					break;
 				}
 			}
+			
 		}
 		
 		show_mouse(buffer);
@@ -330,9 +339,10 @@ void menu (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras)
 
 void letroca (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras, int tempo)
 {
-	struct maior palavraMaior;
+	struct maior palavraMaior, palavraTemp;
 	struct subgrupos *palavras;
-	int i, j, tam, quantas;
+	int i, j, tam, quantas, pressed, usadas, descobertas, desistir;
+	char letra;
 	
 	srand(time(NULL));
 	
@@ -344,10 +354,12 @@ void letroca (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras, int tempo)
 	for ( ; i < tam ; i++ )
 	{
 		palavraMaior.ligado[i] = 1;
+		palavraTemp.ligado[i] = 0;
 	}
 	palavraMaior.ligado[i] = 0;
+	palavraTemp.ligado[i] = 0;
 	
-	quantas = 6;
+	quantas = 7;
 	/* 
 	pipa 
 	lepra
@@ -367,30 +379,30 @@ void letroca (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras, int tempo)
 	
 	for ( i = 0 ; i < quantas ; i++ )
 	{
-		for ( j = 0 ; j < 15 ; j++ )
-		{
-			palavras[i].ligado[j] = 0;
-		}
+		palavras[i].ligado = 0;
 	}
 	
-	sprintf(palavras[0].palavra , "Lepra");
+	sprintf(palavras[0].palavra , "LEPRA");
 	palavras[0].numletras = 5;
-	sprintf(palavras[1].palavra , "Para");
+	sprintf(palavras[1].palavra , "PARA");
 	palavras[1].numletras = 4;
-	sprintf(palavras[2].palavra , "Paralelo");
+	sprintf(palavras[2].palavra , "PARALELO");
 	palavras[2].numletras = 8;
-	sprintf(palavras[3].palavra , "Pelo");
+	sprintf(palavras[3].palavra , "PELO");
 	palavras[3].numletras = 4;
-	sprintf(palavras[4].palavra , "Ele");
-	palavras[4].numletras = 3; 
-	sprintf(palavras[5].palavra , "Ela");
+	sprintf(palavras[4].palavra , "RALO");
+	palavras[4].numletras = 4; 
+	sprintf(palavras[5].palavra , "ELA");
 	palavras[5].numletras = 3;
-	sprintf(palavras[6].palavra , "Ler");
+	sprintf(palavras[6].palavra , "LER");
 	palavras[6].numletras = 3;
 	
 	qsort(palavras, quantas, sizeof(struct subgrupos),subgrupoSort);
 	
 	embaralhaFunc(&palavraMaior,tam);
+	
+	clear_keybuf();
+	usadas = descobertas = desistir = 0;
 	
 	if ( tempo )
 	{
@@ -401,8 +413,8 @@ void letroca (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras, int tempo)
 			install_int(dec_tim_tempo, 1000);
 	}
 	
-	/*while ( 1 )
-	{*/
+	while ( 1 )
+	{
 		blit(fundo, buffer, 0, 0, 0, 0, fundo->w, fundo->h);
 		for ( i = 0 ; i < tam ; i++ )
 		{
@@ -415,19 +427,186 @@ void letroca (BITMAP *buffer, BITMAP *fundo, BITMAP **Letras, int tempo)
 			{
 				rect(buffer,15+((Letras[0]->w+2)*i),buffer->h-65,15+((Letras[0]->w+2)*(i+1)),buffer->h-15,makecol(255,255,255));
 			}
+			
 			/* Desenha a palavra selecionada */
-			if ( !palavraMaior.ligado[i] )
+			if ( palavraTemp.ligado[i] )
 			{
-				draw_sprite(buffer , Letras[ (palavraMaior.palavra[i] - 'A') ] , 15+((Letras[0]->w+2)*i) , buffer->h-65);
+				draw_sprite(buffer , Letras[ (palavraTemp.palavra[i] - 'A') ] , 15+((Letras[0]->w+2)*i) , buffer->h-115);
 			}
 			else
 			{
 				rect(buffer,15+((Letras[0]->w+2)*i),buffer->h-115,15+((Letras[0]->w+2)*(i+1)),buffer->h-70,makecol(255,255,255));
 			}
 		}
+		
+		for ( i = 0 ; i < quantas && i < 8 ; i++ )
+		{
+			if ( palavras[i].ligado )
+			{
+				for ( j = 0 ; palavras[i].palavra[j] ; j++ )
+				{
+					stretch_sprite(buffer, Letras[ (palavras[i].palavra[j] - 'A') ] , (15+(25+2)*j) , (15+(30*i)), 25, 25);
+				}
+			}
+			else
+			{
+				for ( j = 0 ; palavras[i].palavra[j] ; j++ )
+				{
+					rect(buffer, 15+((25+2)*j), 15+(30*i), 15+((25+2)*(j+1)), 15+(30*i)+25, makecol(255,255,255));
+				}
+			}
+		}
+		
+		if ( quantas > 8 )
+		{
+			for ( i = 8 ; i < quantas ; i++ )
+			{
+				if ( palavras[i].ligado )
+				{
+					for ( j = 0 ; palavras[i].palavra[j] ; j++ )
+					{
+						stretch_sprite(buffer, Letras[ (palavras[i].palavra[j] - 'A') ] , (240+(25+2)*j) , (15+(30*(i-8))), 25, 25);
+					}
+				}
+				else
+				{
+					for ( j = 0 ; palavras[i].palavra[j] ; j++ )
+					{
+						rect(buffer, 240+((25+2)*j), 15+(30*(i-8)), 240+((25+2)*(j+1)), 15+(30*(i-8))+25, makecol(255,255,255));
+					}
+				}
+			}
+		}
+		
+		text_mode(-1);
+		
+		/* Caixa de Score */
+		rectfill(buffer, buffer->w-100, 15, buffer->w-15, 50, makecol(0,0,0));
+		textprintf_centre(buffer, font, buffer->w-57, 30, makecol(255,255,255),
+		"%05d", score );
+		
+		/* Caixa de tempo */
+		if ( tempo )
+		{
+			rectfill(buffer, buffer->w-100, 65, buffer->w-15, 100, makecol(0,0,0));
+			textprintf_centre(buffer, font, buffer->w-57, 80, makecol(255,255,255),
+			"%02d:%02d", tim_tempo/60, tim_tempo%60 );
+		}
+		
 		blit(buffer, screen, 0, 0, 0, 0, buffer->w, buffer->h);
-		getchar();
-	/*}*/
+		if ( keypressed() ) 
+		{ 
+			pressed = readkey();
+			
+			letra = (pressed & 0xFF);
+			if ( (pressed >> 8) == KEY_BACKSPACE && usadas > 0)
+			{
+				usadas--;
+				palavraMaior.ligado[palavraTemp.ligado[usadas]-1] = 1;
+				palavraTemp.ligado[usadas] = 0;
+			}
+			if ( (pressed >> 8) == KEY_ENTER || (pressed >> 8) == KEY_ENTER_PAD )
+			{
+				/* Vê se tem mais de três letras digitadas */
+				if ( usadas >= 3 )
+				{
+					/* Varre todas as palavras com número de letra menor ou igual ao de
+					   letras digitadas */
+					for ( i = 0 ; palavras[i].numletras <= usadas && i < quantas ; i++ )
+					{
+						/* Caso a palavra ainda não esteja aparecendo */
+						if ( !palavras[i].ligado )
+						{
+							/* Verifica letra a letra quais são iguais, quando encontra 
+							   uma diferente, sai do For */
+							for ( j = 0 ; j < usadas ; j++ )
+							{
+								if ( palavraTemp.palavra[j] != palavras[i].palavra[j] )
+								{
+									break;
+								}
+							}
+							/* Se o número de letras iguais for igual ao número de letras
+							   da palavra que está buscando e se for igual ao de letras 
+							   digitadas */
+							if ( j == palavras[i].numletras && j == usadas )
+							{
+								palavras[i].ligado = 1;
+								score += 100 * j;
+								/* Varre as letras digitadas desligando-as e religando a 
+								   palavra embaralhada */
+								for ( j = 0 ; j < usadas ; j++ )
+								{
+									palavraMaior.ligado[ palavraTemp.ligado[j]-1 ] = 1;
+									palavraTemp.ligado[j] = 0;
+								}
+								usadas = 0;
+								descobertas++;
+								/* Mensagem de encontrada */
+							}
+							else
+							{
+								/* Mensagem de não encontrada */
+							}
+						}
+						else
+						{
+							/* Mensagem de já existe */
+						}
+					}
+				}
+				else
+				{
+					/* Mensagem de não encontrada, pois digitadas < 3 */
+				}
+			}
+			if ( (pressed >> 8) == KEY_ESC )
+			{
+				/* caixinha: você deseja mesmo sair? xP */
+				desistir = 1;
+			}
+			if ( (pressed >> 8) == KEY_SPACE )
+			{
+				for ( i = 0 ; i < usadas ; i++ )
+				{
+					palavraMaior.ligado[ palavraTemp.ligado[i]-1 ] = 1;
+					palavraTemp.ligado[i] = 0;
+				}
+				usadas = 0;
+				embaralhaFunc(&palavraMaior,tam);
+			}
+			for ( i = 0 ; i < tam ; i++ )
+			{
+				if ( toupper(letra) == palavraMaior.palavra[i] && palavraMaior.ligado[i] )
+				{
+					palavraMaior.ligado[i] = 0;
+					palavraTemp.ligado[usadas] = i+1;
+					palavraTemp.palavra[usadas] = toupper(letra);
+					usadas++;
+					break;
+				}
+			} 
+		}
+		
+		if ( desistir )
+		{
+			/* Desistiu! */
+			score += tim_tempo * descobertas;
+			break;
+		}
+		if ( descobertas == quantas )
+		{
+			/* Fim de jogo */
+			score += tim_tempo * descobertas;
+			break;
+		}
+		
+		if ( tim_tempo < 0 && tempo )
+		{
+			/* Ah.. cabar xP */
+			break;
+		}
+	}
 }
 
 int subgrupoSort (const void *ant, const void *post)
